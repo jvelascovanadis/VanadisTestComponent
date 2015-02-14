e45204e6a091a07d59b54f503c26866c30bf6484
jvelasco <jvelasco@vanadis.es>
2015-02-14 19:00:54 +0100

    First updates

---
+++ VanadisDemoFramework/VanadisDemoFramework/VanadisDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  VanadisDemoFramework.h
+//  VanadisDemoFramework
+//
+//  Created by jorge Velasco on 14/02/15.
+//  Copyright (c) 2015 Vanadis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VanadisDemoFramework.
+FOUNDATION_EXPORT double VanadisDemoFrameworkVersionNumber;
+
+//! Project version string for VanadisDemoFramework.
+FOUNDATION_EXPORT const unsigned char VanadisDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VanadisDemoFramework/PublicHeader.h>
+
+
