6138eb18895febffeca10a567047359e1c38dd2d
jvelasco <jvelasco@vanadis.es>
2015-02-14 19:00:08 +0100

    Added Person file

---
+++ VanadisDemoFramework/VanadisDemoFramework/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  VanadisDemoFramework
+//
+//  Created by jorge Velasco on 14/02/15.
+//  Copyright (c) 2015 Vanadis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ VanadisDemoFramework/VanadisDemoFramework/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  VanadisDemoFramework
+//
+//  Created by jorge Velasco on 14/02/15.
+//  Copyright (c) 2015 Vanadis. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
